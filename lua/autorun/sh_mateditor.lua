IJWTB/material-editor-2

commit 9e1154a18e98f52f3617cae44ba96a557defaec2

Added TreeSway parameters
Speed, Strength, Height, StartHeight, Radius, StartRadius, Leaf Speed, Leaf Strength

lua/autorun/sh_mateditor.lua
@@ -51,6 +51,14 @@ function advMats:Set(ent, texture, data, submatid)
 			PhongBoost = data.PhongBoost or 1,
 			PhongFresnel = data.PhongFresnel or "0 0.5 1",
 			UseTSway = data.UseTSway or 0,
+			TreeSwaySpeed = data.TreeSwaySpeed or 1,
+			TreeSwayStrength = data.TreeSwayStrength or 0.1,
+			TreeSwayStartHeight = data.TreeSwayStartHeight or 0.1,
+			TreeSwayHeight = data.TreeSwayHeight or 300,
+			TreeSwayStartRadius = data.TreeSwayStartRadius or 0.1,
+			TreeSwayRadius = data.TreeSwayRadius or 100,
+			TreeLeafSpeed = data.TreeLeafSpeed or 0.1,
+			TreeLeafStrength = data.TreeLeafStrength or 0.1,
 			AlphaType = data.AlphaType or 0,
 			NoCull = data.NoCull or 0
 		}
@@ -109,7 +117,7 @@ function advMats:Set(ent, texture, data, submatid)
 		end
 		
 		if	(data.UseTSway) then
-			uid = uid .. (data.UseTSway)
+			uid = uid .. (data.UseTSway) .. "+" .. (data.TreeSwaySpeed or 1) .. "+" .. (data.TreeSwayStrength or 0.1) .. "+" .. (data.TreeLeafSpeed or 0.1) .. "+" .. (data.TreeLeafStrength or 0.1) .. "+" .. (data.TreeSwayStartHeight or 0.1) .. "+" .. (data.TreeSwayHeight or 300) .. "+" .. (data.TreeSwayStartRadius or 0.1) .. "+" .. (data.TreeSwayRadius or 100)
 		end
 
 		uid = uid:gsub("%.", "-")
@@ -163,6 +171,14 @@ function advMats:Set(ent, texture, data, submatid)
 		data.PhongBoost = data.PhongBoost or 1
 		data.PhongFresnel = data.PhongFresnel or "0 0.5 1"
 		data.UseTSway = data.UseTSway or 0
+		data.TreeSwaySpeed = data.TreeSwaySpeed or 1
+		data.TreeSwayStrength = data.TreeSwayStrength or 0.1
+		data.TreeSwayStartHeight = data.TreeSwayStartHeight or 0.1
+		data.TreeSwayHeight = data.TreeSwayHeight or 300
+		data.TreeSwayStartRadius = data.TreeSwayStartRadius or 0.1
+		data.TreeSwayRadius = data.TreeSwayRadius or 100
+		data.TreeLeafSpeed = data.TreeLeafSpeed or 0.1
+		data.TreeLeafStrength = data.TreeLeafStrength or 0.1
 		data.AlphaType = data.AlphaType or 0
 		data.NoCull = data.NoCull or 0
 
@@ -202,7 +218,7 @@ function advMats:Set(ent, texture, data, submatid)
 		end
 		
 		if	(data.UseTSway) then
-			uid = uid .. (data.UseTSway)
+			uid = uid .. (data.UseTSway) .. "+" .. data.TreeSwaySpeed .. "+" .. data.TreeSwayStrength .. data.TreeLeafSpeed .. "+" .. data.TreeLeafStrength .. "+" .. data.TreeSwayStartHeight .. "+" .. data.TreeSwayHeight .. "+" .. data.TreeSwayStartRadius .. "+" .. data.TreeSwayRadius
 		end
 
 		uid = uid:gsub("%.", "-")
@@ -276,6 +292,18 @@ function advMats:Set(ent, texture, data, submatid)
 			
 			if(data.UseTSway > 0) then
 				matTable["$treesway"] = data.UseTSway
+				matTable["$treeSwaySpeed"] = data.TreeSwaySpeed
+				matTable["$treeSwayStrength"] = data.TreeSwayStrength
+				matTable["$treeSwayScrumbleSpeed"] = data.TreeLeafSpeed
+				matTable["$treeSwayScrumbleStrength"] = data.TreeLeafStrength
+				if data.UseTSway == 1 then
+					matTable["$treeSwayHeight"] = data.TreeSwayHeight
+					matTable["$treeSwayStartHeight"] = data.TreeSwayStartHeight
+				elseif data.UseTSway == 2 then
+					matTable["$treeSwayRadius"] = data.TreeSwayRadius
+					matTable["$treeSwayStartRadius"] = data.TreeSwayStartRadius
+				end
+				
 				matTable["$treeSwayStatic"] = 1 -- Only static treesway, cus I don't wanna deal with hundreds of parameters, that env_wind may or may not fuck up anyway.
 				-- If $treeswaystaticvalues is added to gmod, put that in here
 			end
@@ -327,6 +355,14 @@ function advMats:Set(ent, texture, data, submatid)
 			PhongBoost 	= data.PhongBoost or 1,
 			PhongFresnel = data.PhongFresnel or "0 0.5 1",
 			UseTSway 	= data.UseTSway or 0,
+			TreeSwaySpeed = data.TreeSwaySpeed or 1,
+			TreeSwayStrength = data.TreeSwayStrength or 0.1,
+			TreeSwayStartHeight = data.TreeSwayStartHeight or 0.1,
+			TreeSwayHeight = data.TreeSwayHeight or 300,
+			TreeSwayStartRadius = data.TreeSwayStartRadius or 0.1,
+			TreeSwayRadius = data.TreeSwayRadius or 100,
+			TreeLeafSpeed = data.TreeLeafSpeed or 0.1,
+			TreeLeafStrength = data.TreeLeafStrength or 0.1,
 			AlphaType = data.AlphaType or 0
 		}
 		if submatid == -1 and IsValid(ent) then

lua/weapons/gmod_tool/stools/advmat.lua
@@ -28,6 +28,14 @@ TOOL.ClientConVar["usephong"] = "0"
 TOOL.ClientConVar["phongboost"] = "1"
 TOOL.ClientConVar["phongfresnel"] = "0, 0.5, 1"
 TOOL.ClientConVar["usetreesway"] = "0"
+TOOL.ClientConVar["treeswayspeed"] = "1"
+TOOL.ClientConVar["treeswaystrength"] = "0.1"
+TOOL.ClientConVar["treeswaystartheight"] = "0.1"
+TOOL.ClientConVar["treeswayheight"] = "300"
+TOOL.ClientConVar["treeswaystartradius"] = "0.1"
+TOOL.ClientConVar["treeswayradius"] = "100"
+TOOL.ClientConVar["treeleafspeed"] = "0.1"
+TOOL.ClientConVar["treeleafstrength"] = "0.1"
 TOOL.ClientConVar["alphatype"] = "0"
 TOOL.ClientConVar["nocull"] = "0"
 
@@ -84,6 +92,14 @@ function TOOL:LeftClick(trace)
 	local phongboost = tonumber(self:GetClientInfo("phongboost"))
 	local phongfresnel = self:GetClientInfo("phongfresnel")
 	local usetreesway = tonumber(self:GetClientInfo("usetreesway"))
+	local treeswayspeed = tonumber(self:GetClientInfo("treeswayspeed"))
+	local treeswaystrength = tonumber(self:GetClientInfo("treeswaystrength"))
+	local treeswaystartheight = tonumber(self:GetClientInfo("treeswaystartheight"))
+	local treeswayradius = tonumber(self:GetClientInfo("treeswayradius"))
+	local treeswaystartradius = tonumber(self:GetClientInfo("treeswaystartradius"))
+	local treeswayheight = tonumber(self:GetClientInfo("treeswayheight"))
+	local treeleafspeed = tonumber(self:GetClientInfo("treeleafspeed"))
+	local treeleafstrength = tonumber(self:GetClientInfo("treeleafstrength"))
 	local alphatype = tonumber(self:GetClientInfo("alphatype"))
 	local nocull = tonumber(self:GetClientInfo("nocull"))
 	
@@ -115,6 +131,14 @@ function TOOL:LeftClick(trace)
 		PhongBoost = phongboost,
 		PhongFresnel = phongfresnel,
 		UseTSway = usetreesway,
+		TreeSwaySpeed = treeswayspeed,
+		TreeSwayStrength = treeswaystrength,
+		TreeSwayStartHeight = treeswaystartheight,
+		TreeSwayHeight = treeswayheight,
+		TreeSwayStartRadius = treeswaystartradius,
+		TreeSwayRadius = treeswayradius,
+		TreeLeafSpeed = treeleafspeed,
+		TreeLeafStrength = treeleafstrength,
 		AlphaType = alphatype,
 		NoCull = nocull
 	}, submatid)
@@ -368,7 +392,27 @@ do
 		tscombox:AddChoice("None", 0)
 		tscombox:AddChoice("Classic", 1)
 		tscombox:AddChoice("Radial", 2)
-		CPanel:ControlHelp("If enabled, your prop will sway according to the sway type.")
+		CPanel:ControlHelp("If enabled, your prop will sway according to the sway type. DOES NOT WORK WITH BUMPMAPS!")
+		CPanel:NumSlider("#tool.advmat.treeswayspeed", "advmat_treeswayspeed", 0, 100)
+		CPanel:ControlHelp("How quickly the tree sways (as a multiplier of time)")
+		CPanel:NumSlider("#tool.advmat.treeswaystrength", "advmat_treeswaystrength", 0, 500)
+		CPanel:ControlHelp("How much the tree sways (as a multiplier of position)")
+		CPanel:NumSlider("#tool.advmat.treeleafspeed", "advmat_treeleafspeed", 0, 100)
+		CPanel:ControlHelp("How quickly the leaves move (as a multiplier of time)")
+		CPanel:NumSlider("#tool.advmat.treeleafstrength", "advmat_treeleafstrength", 0, 500)
+		CPanel:ControlHelp("How much the leaves move (as a multiplier of position)")
+		
+		CPanel:Help("Classic TS Settings")
+		CPanel:NumSlider("#tool.advmat.treeswaystartheight", "advmat_treeswaystartheight", 0, 1)
+		CPanel:ControlHelp("Portion of the height of the model at which the effect starts to fade in. (0 to 1)")
+		CPanel:NumSlider("#tool.advmat.treeswayheight", "advmat_treeswayheight", 0, 10000)
+		CPanel:ControlHelp("Height at which the effect is on fully (in world units)")
+		
+		CPanel:Help("Radial TS Settings")
+		CPanel:NumSlider("#tool.advmat.treeswaystartradius", "advmat_treeswaystartradius", 0, 1)
+		CPanel:ControlHelp("Portion of radius at which the effect starts to fade in (0 to 1)")
+		CPanel:NumSlider("#tool.advmat.treeswayradius", "advmat_treeswayradius", 0, 10000)
+		CPanel:ControlHelp("Radius at which the effect is on fully (in world units)")
 		
 		local alphabox, alphalab = CPanel:ComboBox("Alpha Type", "advmat_alphatype")
 		alphabox:AddChoice("None", 0)
@@ -407,6 +451,15 @@ if (CLIENT) then
 	language.Add("tool.advmat.rotate", "Rotation")
 	language.Add("tool.advmat.usenoise", "Use noise texture")
 	language.Add("tool.advmat.submatid", "SubMaterial ID")
+	
+	language.Add("tool.advmat.treeswayspeed", "Sway Speed")
+	language.Add("tool.advmat.treeswaystrength", "Sway Strength")
+	language.Add("tool.advmat.treeswaystartheight", "Sway Start Height")
+	language.Add("tool.advmat.treeswayheight", "Sway Height")
+	language.Add("tool.advmat.treeswaystartradius", "Sway Start Radius")
+	language.Add("tool.advmat.treeswayradius", "Sway Radius")
+	language.Add("tool.advmat.treeleafspeed", "Leaf Speed")
+	language.Add("tool.advmat.treeleafstrength", "Leaf Strength")
 	
 	language.Add("tool.advmat.nocull", "No Cull")
 

commit 8046e28e56d04a808332139180a9960329d58fab

Fixed breaking advmat2 for everything but singleplayer because of a typo
Bug introduced in https://github.com/ZH-Hristov/GMOD-Material-Editor-2/commit/9e1154a18e98f52f3617cae44ba96a557defaec2#diff-ba3deb473dd53b89625d05fa991dba12b5306b93400016e02d8f2af9678c3e2fR221

Had nothing to do with multiplayer itself. You just had a typo that made the sever's UID not match the client's UID. A single character typo.
If you're not going to bother looking into something so simple to fix entire non-singleplayer functionality, why are you working on this at all?

lua/autorun/sh_mateditor.lua
@@ -218,7 +218,7 @@ function advMats:Set(ent, texture, data, submatid)
 		end
 		
 		if	(data.UseTreeSway) then
-			uid = uid .. (data.UseTreeSway) .. "+" .. data.TreeSwaySpeed .. "+" .. data.TreeSwayStrength .. data.TreeLeafSpeed .. "+" .. data.TreeLeafStrength .. "+" .. data.TreeSwayStartHeight .. "+" .. data.TreeSwayHeight .. "+" .. data.TreeSwayStartRadius .. "+" .. data.TreeSwayRadius
+			uid = uid .. (data.UseTreeSway) .. "+" .. data.TreeSwaySpeed .. "+" .. data.TreeSwayStrength .. "+" .. data.TreeLeafSpeed .. "+" .. data.TreeLeafStrength .. "+" .. data.TreeSwayStartHeight .. "+" .. data.TreeSwayHeight .. "+" .. data.TreeSwayStartRadius .. "+" .. data.TreeSwayRadius
 		end
 
 		uid = uid:gsub("%.", "-")